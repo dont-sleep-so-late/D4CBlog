f7b8a85d18d86b8282284b82a6d39de3fe379209
你别睡这么晚 <1921909953@qq.com>
2023-04-06 16:31:12 +0800

    addREADME

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 # 推送代码到github和Gitee
 git push
-# git push git@github.com:BruceCQM/bruceblog.git main:main
-git push git@gitee.com:brucecai55520/bruceblog.git main:bruceblog_vitepress
+# git push git@github.com:dont-sleep-so-late/D4CBlog.git 
+git push git@gitee.com:dont-sleep-so-late/D4CBlog.git 
